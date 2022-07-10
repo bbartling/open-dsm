8576eb1f4c0ed7e75b733498e0b3f359802932cf
Ben Bartling <ben.bartling@gmail.com>
2022-07-10 14:29:44 -0500

    testing rtu

--- Load-Shed/rtu_clg_compressors/main.py
+++ Load-Shed/rtu_clg_compressors/main.py
@@ -3,58 +3,101 @@ import logging
 from datetime import datetime
 
 
-
-
 # datetime object containing current date and time
 now = datetime.now()
 
-dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-logging.basicConfig(filename=f'log_{dt_string}.log', level=logging.WARNING)
+dt_string = now.strftime("%m_%d_%Y %H:%M:%S")
+logging.basicConfig(filename=f'log_{dt_string}.log', level=logging.INFO)
+
+print(f"Running NOW date and time is {dt_string}")
 
 
-print("Running NOW date and time is", dt_string)
-logging.info(f"Running NOW date and time is {dt_string}")
-
-
-# define BACnet app
+# define bacnet app
 bacnet = BAC0.lite()
+
+object_type_and_instance = [
+                "binaryOutput 3", #stage 1
+                "binaryOutput 4", #stage 2
+                "binaryOutput 5", #stage 3
+                "binaryOutput 6"  #stage 4
+                ]
 
 
 # Duct Pressure RTU Point
 address = "10.200.200.27"
-object_type = "analogValue"
-object_instance = "3"
-priority = 7 # BACnet priority to write
-value = 0.5 # new duct pressure setpoint
+priority = 8
+
+
+# compressor read values
+on_value = "active"
+off_value = "inactive"
+
 
 # Event duration
 event_duration_minutes = 10
 
 
-read_vals = f"{address} {object_type} {object_instance} presentValue"
-check = bacnet.read(read_vals)
-print(f"Initial RTU duct pressure is: {check}")
-logging.info(f"Initial RTU duct pressure is: {check}")
+# make requests to count clg stages
+stages_running_data = []
+for stage in object_type_and_instance:
+    read_req = f"{address} {stage} presentValue"
+    read_val = bacnet.read(read_req)
+    print("stage ", object_type_and_instance, "is: ",read_val)
+    stages_running_data.append(read_val)
 
 
-
-# create write statement and run
-write_vals = f"{address} {object_type} {object_instance} presentValue {value} - {priority}"
-print(f"Excecuting BACnet write: {write_vals}")
-logging.info(f"Excecuting BACnet write: {write_vals}")
-
-
-
-bacnet.write(write_vals)
-print(f"BACnet written to {address} device")
-logging.info(f"BACnet written to {address} device")
+# loop thru read request data and count stages
+stages_running = 0
+for stage in stages_running_data:    
+    if stage == 'active':
+        stages_running += 1
 
 
+print(f"the stages running: {stages_running}")
+logging.info(f"the stages running: {stages_running}")
 
-read_vals_two = f"{address} {object_type} {object_instance} presentValue"
-check_two = bacnet.read(read_vals_two)
-print(f"Checking the BACnet write to make sure it took: {check_two}")
-logging.info(f"Checking the BACnet write to make sure it took: {check_two}")
+# make some decisions based on how many stages running
+if stages_running == 0:
+    print(f"No compressors are running, passing....")
+    logging.info("No compressors are running, passing....")
+
+
+# stage 1, 2, 3 are ON and 4 is OFF
+#elif self.num_of_clg_stages_active == self.num_of_clg_stages - 1:
+elif stages_running == 3:
+    print(f"3 stages of cooling are running, overriding stage 3 and 4 OFF....")
+    logging.info("3 stages of cooling are running, overriding stage 3 and 4 OFF....")            
+
+    # create write statement and run
+    write_stage_3_off = f"{address} binaryOutput 5 presentValue {off_value} - {priority}"
+    print("Excecuting BACnet write: ", write_stage_3_off)
+    logging.info("Excecuting BACnet write: ", write_stage_3_off) 
+    bacnet.write(write_stage_3_off)
+
+
+    # create write statement and run
+    write_stage_4_off = f"{address} binaryOutput 6 presentValue {off_value} - {priority}"
+    print("Excecuting BACnet write: ", write_stage_4_off)
+    logging.info("Excecuting BACnet write: ", write_stage_4_off) 
+    bacnet.write(write_stage_4_off)
+
+
+elif stages_running == 4:
+    print(f"4 stages of cooling are running, overriding stage ONLY stage 4 OFF....")
+    logging.info("4 stages of cooling are running, overriding stage ONLY stage 4 OFF....")   
+
+
+    # create write statement and run
+    write_stage_4_off = f"{address} binaryOutput 6 presentValue {off_value} - {priority}"
+    print("Excecuting BACnet write: ", write_stage_4_off)
+    logging.info("Excecuting BACnet write: ", write_stage_4_off) 
+    bacnet.write(write_stage_4_off)
+
+
+else:
+    print(f"Not enough cooling running to override OFF")
+    logging.info("Not enough cooling running to override OFF")   
+
 
 
 
@@ -63,35 +106,23 @@ HALF TIME
 """
 
 
-
 print(f"sleeping now... for in minutes: {event_duration_minutes * 60}")
-logging.info(f"sleeping now... for in minutes: {event_duration_minutes * 60}")
+logging.info(f"sleeping now... for in minutes: {event_duration_minutes * 60}") 
 time.sleep(event_duration_minutes * 60)
 
 
-
-print(f"time to release!!!")
-logging.info(f"time to release!!!")
-release_vals = f"{address} {object_type} {object_instance} presentValue null - {priority}"
-print(f"Excecuting BACnet release: {release_vals}")
-logging.info(f"Excecuting BACnet release: {release_vals}")
+print("time to release!!!")
+logging.info("time to release!!!")
 
 
+# release stage 3 and 4 stages
+release_req_stag_3 = f"{address} binaryOutput 5 presentValue null - {priority}"
+bacnet.write(release_req_stag_3)
 
-bacnet.write(release_vals)
-print(f"BACnet release to {address} device sucess")
-logging.info(f"BACnet release to {address} device sucess")
-
-
-
-release_check = f"{address} {object_type} {object_instance} presentValue"
-check_= bacnet.read(release_check)
-print(f"Checking the BACnet release was good: {check_}")
-logging.info(f"Checking the BACnet release was good: {check_}")
+release_req_stag_4 = f"{address} binaryOutput 6 presentValue null - {priority}"
+bacnet.write(release_req_stag_4)
 
 
-
-print(f"all done BAC0 rocks....!")
-logging.info(f"all done BAC0 rocks....!")
-
+print("all done BAC0 rocks....!")
+logging.info("all done BAC0 rocks....!")
 bacnet.disconnect()
--- Load-Shed/rtu_duct_pressure/main.py
+++ Load-Shed/rtu_duct_pressure/main.py
@@ -1,6 +1,17 @@
 import BAC0, time
+import logging
+from datetime import datetime
 
 
+# datetime object containing current date and time
+now = datetime.now()
+
+dt_string = now.strftime("%m_%d_%Y %H:%M:%S")
+logging.basicConfig(filename=f'log_{dt_string}.log', level=logging.INFO)
+
+print(f"Running NOW date and time is {dt_string}")
+
+# define bacnet app
 bacnet = BAC0.lite()
 
 # Duct Pressure RTU Point
@@ -11,7 +22,7 @@ priority = 8
 value = 0.5
 
 # Event duration
-event_duration_minutes = 10
+event_duration_minutes = 5
 
 
 read_vals = f"{address} {object_type} {object_instance} presentValue"
@@ -21,33 +32,43 @@ print(f"Initial RTU duct pressure is: {check}")
 
 # create write statement and run
 write_vals = f"{address} {object_type} {object_instance} presentValue {value} - {priority}"
-print("Excecuting BACnet write: ", write_vals)
+print(f"Excecuting BACnet write: {write_vals}")
+
 
 write_result = bacnet.write(write_vals)
 print(f"BACnet written to {address} device")
 
+
 read_vals_two = f"{address} {object_type} {object_instance} presentValue"
 check_two = bacnet.read(read_vals_two)
-print("Checking the BACnet write to make sure it took: ", check_two)
+print(f"Checking the BACnet write to make sure it took: {check_two}")
+
 
 
 """
 HALF TIME
 """
       
-print("sleeping now... for in minutes: ",event_duration_minutes * 60)
+print(f"sleeping now... for in minutes: {event_duration_minutes * 60}")
+logging.info(f"sleeping now... for in minutes: {event_duration_minutes * 60}")
 time.sleep(event_duration_minutes * 60)
 
-print("time to release!!!")
+
+print(f"time to release!!!")
+logging.info("time to release!!!")
 release_vals = f"{address} {object_type} {object_instance} presentValue null - {priority}"
-print("Excecuting BACnet release: ", release_vals)
+print(f"Excecuting BACnet release: {release_vals}")
+
 
 release_result = bacnet.write(release_vals)
-print("BACnet release to {address} device")
+print(f"BACnet release to {address} device")
+
 
 release_check = f"{address} {object_type} {object_instance} presentValue"
 check_= bacnet.read(release_check)
-print("Checking the BACnet release was good: ", check_)
 
-print("all done BAC0 rocks....!")
+
+
+print(f"all done BAC0 rocks....!")
+logging.info(f"all done BAC0 rocks....!")
 bacnet.disconnect()
